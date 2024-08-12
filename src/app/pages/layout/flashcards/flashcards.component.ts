60ad6d2ce39d03c233e6452a905a4129f78691c1
mtri <TRI.VM06567@sinhvien.hoasen.edu.vn>
2024-08-12 22:35:53 +0700

    commit for merge

--- src/app/pages/layout/flashcards/flashcards.component.ts
+++ src/app/pages/layout/flashcards/flashcards.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../shared/modules/material.module';
+import { SharedModule } from '../../../shared/modules/shared.module';
 
 @Component({
   selector: 'app-flashcards',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, SharedModule],
   templateUrl: './flashcards.component.html',
-  styleUrl: './flashcards.component.scss'
+  styleUrl: './flashcards.component.scss',
 })
-export class FlashcardsComponent {
-
-}
+export class FlashcardsComponent {}
