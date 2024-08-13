2f01880012ebce1458717ba5e8799c27947644cd
Quan <tdquan2112@gmail.com>
2024-08-13 16:34:14 +0700

    function add to match and learn still on progress not fully functional

--- src/app/pages/study-modes/learn/learn.component.ts
+++ src/app/pages/study-modes/learn/learn.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import {ProgressBarComponent} from "./components/progress-bar/progress-bar.component";
 import {MultipleChoiceComponent} from "./components/multiple-choice/multiple-choice.component";
 
 @Component({
   selector: 'app-learn',
   standalone: true,
   imports: [
-    ProgressBarComponent,
     MultipleChoiceComponent
   ],
   templateUrl: './learn.component.html',
