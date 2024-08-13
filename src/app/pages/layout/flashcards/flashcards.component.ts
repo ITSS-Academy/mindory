e21f340cabd7b5d4a2b0e5474ecce3e43d5aa54c
p.anh <phuocanh554@gmail.com>
2024-08-13 16:42:09 +0700

    update page flashcard

--- src/app/pages/layout/flashcards/flashcards.component.ts
+++ src/app/pages/layout/flashcards/flashcards.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {TopicHierarchyComponent} from "./components/topic-hierarchy/topic-hierarchy.component";
 import {RegimeComponent} from "./components/regime/regime.component";
 import {LessonContentComponent} from "./components/lesson-content/lesson-content.component";
-import {PaginationControlComponent} from "./components/pagination-control/pagination-control.component";
+import {PaginationControlComponent} from "./components/lesson-content/pagination-control/pagination-control.component";
 import {ViewComponent} from "./components/view/view.component";
 
 @Component({
