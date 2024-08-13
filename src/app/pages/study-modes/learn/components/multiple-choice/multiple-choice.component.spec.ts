ac1cd183ee277315c3a1ea11063881a3919e9ebb
Quan <tdquan2112@gmail.com>
2024-08-13 08:53:18 +0700

    learn match test, change and update

---
+++ src/app/pages/study-modes/learn/components/multiple-choice/multiple-choice.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MultipleChoiceComponent } from './multiple-choice.component';
+
+describe('MultipleChoiceComponent', () => {
+  let component: MultipleChoiceComponent;
+  let fixture: ComponentFixture<MultipleChoiceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MultipleChoiceComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MultipleChoiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/study-modes/learn/components/multiple-choice/multiple-choice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiple-choice',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiple-choice.component.html',
+  styleUrl: './multiple-choice.component.scss'
+})
+export class MultipleChoiceComponent {
+
+}
