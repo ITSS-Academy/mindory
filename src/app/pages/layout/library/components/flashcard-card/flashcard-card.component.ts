5a39d617d9dc5dc6fa74f4027337438255fd29c1
mtri <TRI.VM06567@sinhvien.hoasen.edu.vn>
2024-08-15 15:20:12 +0700

    update falshcard in library page

--- src/app/pages/layout/library/components/flashcard-card/flashcard-card.component.ts
+++ src/app/pages/layout/library/components/flashcard-card/flashcard-card.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../../shared/modules/material.module';
+import { SharedModule } from '../../../../../shared/modules/shared.module';
 
 @Component({
   selector: 'app-flashcard-card',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, SharedModule],
   templateUrl: './flashcard-card.component.html',
-  styleUrl: './flashcard-card.component.scss'
+  styleUrl: './flashcard-card.component.scss',
 })
-export class FlashcardCardComponent {
-
-}
+export class FlashcardCardComponent {}
