9caac468eec7e07f0a921f4e52b747d44ae5e27a
mtri <TRI.VM06567@sinhvien.hoasen.edu.vn>
2024-08-15 11:00:21 +0700

    update home page

--- src/app/pages/layout/home/components/flashcard-sets/flashcard-sets.component.ts
+++ src/app/pages/layout/home/components/flashcard-sets/flashcard-sets.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import {MaterialModule} from "../../../../../shared/modules/material.module";
-import {SharedModule} from "../../../../../shared/modules/shared.module";
+import { MaterialModule } from '../../../../../shared/modules/material.module';
+import { SharedModule } from '../../../../../shared/modules/shared.module';
+import {
+  CdkFixedSizeVirtualScroll,
+  ScrollingModule,
+} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-flashcard-sets',
   standalone: true,
-  imports: [MaterialModule, SharedModule],
+  imports: [
+    MaterialModule,
+    SharedModule,
+    CdkFixedSizeVirtualScroll,
+    ScrollingModule,
+  ],
   templateUrl: './flashcard-sets.component.html',
-  styleUrl: './flashcard-sets.component.scss'
+  styleUrl: './flashcard-sets.component.scss',
 })
-export class FlashcardSetsComponent {
-
-}
+export class FlashcardSetsComponent {}
