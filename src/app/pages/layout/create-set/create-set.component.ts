9827f58608132e1b08b902cfc0b249d42f44e1eb
Quan <tdquan2112@gmail.com>
2024-08-22 09:40:36 +0700

    create set UI add

--- src/app/pages/layout/create-set/create-set.component.ts
+++ src/app/pages/layout/create-set/create-set.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MaterialModule} from "../../../shared/modules/material.module";
+import {SharedModule} from "../../../shared/modules/shared.module";
+import {NavbarComponent} from "../../../components/navbar/navbar.component";
+
 
 @Component({
   selector: 'app-create-set',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, SharedModule, NavbarComponent],
   templateUrl: './create-set.component.html',
   styleUrl: './create-set.component.scss'
 })
